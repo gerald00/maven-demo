c802bd542c07193611e7b9eeb77ac31ee2d471dc
gerald00 <ghc47538067@gmail.com>
2017-02-13 12:20:31 +0000

    Maven

--- src/test/java/com/codebind/AppTest.java
+++ src/test/java/com/codebind/AppTest.java
@@ -5,10 +5,18 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class AppTest {
+    
+	/*protected int value1, value2;
+	 protected void setUp(){
+		 
+	      value1 = 3;
+	      value2 = 3;
+	   }
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
+	   @Test
+	 // test method to add two values
+	   public void testAdd(){
+	      double result = value1 + value2;
+	      assertTrue(result == 6);
+	   }*/
 	}
-
-}
